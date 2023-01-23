01467f9c9291b4d371e2aac328b4f21103ce4a01
sanjuny <sanjuny07@gmail.com>
2023-01-23 20:15:55 +0530

    controller intinzialed

---
+++ controllers/openaiController.js
@@ -0,0 +1,5 @@
+const generateimage = async (req, res) => {
+    res.status(200).json({ success: true })
+}
+
+module.exports = { generateimage }
---
+++ routes/openaiRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { generateimage } = require("../controllers/openaiController")
+
+router.post('/generateimage', generateimage)
+
+module.exports = router;
